fab2aadd1df57f7c1bfefd38d07b17724e84c17e
--global <--global>
2025-05-06 18:17:42 +0900

    add: chage_query node for bm25 retrieve #5

--- llm/utils/recipe_graph.py
+++ llm/utils/recipe_graph.py
@@ -26,7 +26,8 @@ from dotenv import load_dotenv
 load_dotenv(r"./env", override=True)
 
 # llm
-llm = ChatOpenAI(model="gpt-4.1-nano")
+mini_llm = ChatOpenAI(model="gpt-4.1-mini")
+nano_llm = ChatOpenAI(model="gpt-4.1-nano")
 
 # embedding_func
 embedding_func = OpenAIEmbeddings(model="text-embedding-3-large")
@@ -76,6 +77,33 @@ class RecipeAgentState(TypedDict):
 
 
 # retrieve
+change_query_template = """
+당신은 문장을 변환하는 전문가야. 주어진 문장을 아래 규칙에 변형해줘.
+
+규칙:
+주어진 문장에서 음식명과 음식재료를 나열하며 알레르기가 있거나 피해야 하는 음식은 재료명 뒤에 제외를 붙인다.
+
+예시:
+- '금양체질이라 딸기를 피하고 싶어요. 소고기를 이용한 음식을 먹고 싶어요' -> '소고기 딸기제외'
+- '소고기랑 마늘을 피하고 싶은데 미역국을 먹고 싶어요' -> '미역국 소고기제외 마늘제외'
+
+문장:
+{query}
+"""
+
+change_query_prompt = PromptTemplate.from_template(change_query_template)
+
+
+def change_query(state: RecipeAgentState):
+    query = state["query"]
+    print(f"input query: {query}")
+    
+    change_query_chain = change_query_prompt | mini_llm | StrOutputParser()
+    response = change_query_chain.invoke({"query": query})
+    print(f"changed query: {response}")
+    return {"query": response}
+
+
 def retrieve(state: RecipeAgentState):
     """
     사용자의 질문에 기반하여 벡터 스토어에서 문서 검색
@@ -98,7 +126,7 @@ def check_recipe_relevance(state: RecipeAgentState):
     context = state["context"]
     # retrieve_context = state["retrieve_context"]
     # chain
-    relevance_chain = doc_relevance_prompt | llm
+    relevance_chain = doc_relevance_prompt | nano_llm
     response = relevance_chain.invoke({"question": query, "documents": context})
     
     if response["Score"] == 1:
@@ -108,6 +136,19 @@ def check_recipe_relevance(state: RecipeAgentState):
     print("checked: no relevent")
     return "no_relevant"
 
+
+duck_web_search_tool = DuckDuckGoSearchRun()
+
+
+def web_search(state: RecipeAgentState):
+    query = state["query"]
+    web_query = f"요리레시피 {query}"
+    result = duck_web_search_tool.invoke(web_query)
+    print("web search result: ")
+    print(result)
+    
+    return {"context": result}
+    # return {"web_search_context": result}
 
 
 recipe_template = """
@@ -132,7 +173,7 @@ def generate(state: RecipeAgentState):
     *** output 에 따라 StrOutputParser() 수정 *** 
     
     """
-    recipe_rag_chain = recipe_prompt | llm | StrOutputParser() 
+    recipe_rag_chain = recipe_prompt | nano_llm | StrOutputParser() 
     response = recipe_rag_chain.invoke({"query": query, "context": context})
     return {"answer": response}
 
@@ -140,9 +181,24 @@ def generate(state: RecipeAgentState):
 graph_builder = StateGraph(RecipeAgentState)
 
 # add node
+graph_builder.add_node("change_query", change_query)
 graph_builder.add_node('retrieve', retrieve)
 graph_builder.add_node('generate', generate)
-graph_builder.add_node
+graph_builder.add_node("web_search", web_search)
+
+graph_builder.add_edge(START, 'change_query')
+graph_builder.add_edge("change_query", "retrieve")
+
+graph_builder.add_conditional_edges(
+    "retrieve",
+    check_recipe_relevance,
+    {
+        "relevant": "generate",
+        "no_relevant": "web_search"
+    }
+)
+graph_builder.add_edge("web_search", "generate")
+graph_builder.add_edge('generate', END)
 
 # add edge
 graph_builder.add_edge(START, 'retrieve')
